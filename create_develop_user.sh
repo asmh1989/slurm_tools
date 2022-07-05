c2bb76863ad0f150085efe901b0f567e82f3ace0
sun <minhua.sun@aixplorerbio.com>
2022-07-05 11:21:32 +0800

    update script

--- create_develop_user.sh
+++ create_develop_user.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
-for item in `cat account.conf`; do
+for item in `cat /public/home/sun/slurm_tools/account.conf`; do
     echo "find account : ${item}"
     uid=`echo $item | cut -d '=' -f 2 `
     account=`echo $item | cut -d '=' -f 1 `
-    cat /etc/passwd | grep "$account:x:$uid" > /dev/null
+    cat /etc/passwd | grep "$account:x" > /dev/null
     if (( $? != 0 )); then
-        sudo useradd -d /home/$1  -u $uid -m $1 -s /bin/bash
+        sudo useradd -d /home/$account  -u $uid -m $account -s /bin/bash
+        # 自动修改密码
+        pp=`date +%s | sha256sum | base64 | head -c 32 ; echo`
+        echo "$account 's password = $pp"
+        echo "$account:$pp" | sudo chpasswd
         sudo chage -E -1 -m -1 -M 90 -I 14 -W 14 $account
         echo "创建 $account 成功!"
     else
         echo "$account 已经存在!"
     fi 
-    sudo usermod -a -G sz_user $account
+    # sudo usermod -a -G sz_user $account
 done
 
