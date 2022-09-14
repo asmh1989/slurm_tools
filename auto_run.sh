bd1771b55406a6b3e61c7da213ebe700c7420db5
sun <minhua.sun@aixplorerbio.com>
2022-09-14 08:53:26 +0800

    add chensheng

--- auto_run.sh
+++ auto_run.sh
@@ -4,5 +4,5 @@ echo "start run at `hostname`"
 cd /public/home/sun/slurm_tools/ 
 
 sudo bash create_slurm_user_without_sacct.sh
-# sudo userdel baoyu
+# sudo userdel wangxiaoyu
 echo "end run at `hostname`"
